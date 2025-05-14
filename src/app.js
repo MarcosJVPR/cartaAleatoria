58d26b5c2e4415f80cdfa2acbe703a664c3bfc1e
MarcosJVPR <perezmarcosjulio@gmail.com>
2025-05-14 03:51:33 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+  const simbolos = ["♦", "♥", "♠", "♣"];
+  const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  const palo = simbolos[Math.floor(Math.random() * simbolos.length)];
+  const valor = numeros[Math.floor(Math.random() * numeros.length)];
+  const color = (palo === "♥" || palo === "♦") ? "red" : "black";
+
+
+    const contenido = document.getElementById("contenido");
+contenido.innerHTML = `
+  <div class="esquina izq" style="color: ${color}">
+    <div class="valor">${valor}</div>
+    <div class="palo">${palo}</div>
+  </div>
+  <div class="esquina der" style="color: ${color}">
+    <div class="valor">${valor}</div>
+    <div class="palo">${palo}</div>
+  </div>
+  <div class="simbolo" style="color: ${color}">
+    ${palo}
+  </div>
+`}
